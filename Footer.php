5ca38978c3317d6b68753e03cc7e3c0d21064c2e
Waris-Al <92109525+Waris-Al@users.noreply.github.com>
2023-03-08 16:55:15 +0000

    maybe now?

--- Footer.php
+++ Footer.php
@@ -3,7 +3,7 @@
     <h6>Contact Us</h6>
 <p> Email: Access4All@example.com </p>
             <div>
-                &copy; March 2023 changes
+                &copy; March 2023 yoyoyo
     </footer>
 </div>
     <!-- Optional JavaScript -->
