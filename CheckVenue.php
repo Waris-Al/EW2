731e92a12c5d914592a5d1f195c65b6befb97d63
Waris-Al <92109525+Waris-Al@users.noreply.github.com>
2023-03-12 16:04:58 +0000

    plop

--- CheckVenue.php
+++ CheckVenue.php
@@ -80,7 +80,7 @@ if(isset($_POST['save'])) {
  
 <body>
     <div class="container">
-    <h2 class="mb-3">Search Filters ftuydk</h2>
+    <h2 class="mb-3">Search Filters therewaswas</h2>
     <br/><br/>
     
     <form class="form-vertical" method="post">
